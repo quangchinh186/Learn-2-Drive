5845b328476a74d0fb1a1696ede1869ebd3654c6
Tien Dung <dung14b@gmail.com>
2023-08-02 18:04:33 +0700

    2 map

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -7,20 +7,50 @@ public class Control : MonoBehaviour
     // Start is called before the first frame update
     public bool isRunning = false;
     public float speed = 20.0f;
+    public bool isMoving = false;
     void Start()
     {
         isRunning = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.W)){
-            isRunning = !isRunning;
-        }
-        if(isRunning){
+    void move() {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
-        
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+
+    private void CarMove()
+    {
+        GameObject moveToObject = GameObject.FindGameObjectWithTag("moveToObject");
+        GameObject car = GameObject.FindGameObjectWithTag("car_1");
+        car.transform.LookAt(moveToObject.transform.position);
+        car.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "wall")
+        {
+            return;
+        }
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        move();     
+        if (isMoving)
+        {
+            CarMove();
+        }
     }
 }
