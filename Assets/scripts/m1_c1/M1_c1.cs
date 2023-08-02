1f9a026c0d5454c49d3855ab5a38d17726176375
Tien Dung <dung14b@gmail.com>
2023-08-03 01:56:07 +0700

    apply multitag for map3, can make car move event when play reach some position

--- Assets/scripts/m1_c1/M1_c1.cs
+++ Assets/scripts/m1_c1/M1_c1.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class M1_c1 : MonoBehaviour
 {
+    private bool car_1Move = false;
+    private void Car1Move()
+    {
+        GameObject moveToObject = GameObject.FindGameObjectWithTag("moveToObject");
+        GameObject[] cars = GameObject.FindGameObjectsWithTag("car_1");
+        foreach (GameObject car in cars)
+        {
+            Debug.Log("counting....");
+            car.transform.LookAt(moveToObject.transform.position);
+            car.transform.Translate(Vector3.forward * 20.0f * Time.deltaTime);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<Tags>(out var tags))
         {
-            Debug.Log("hello");
-            Debug.Log(string.Join(',', tags.All.Select(t => t.Name)));   
+            if (tags.HasTag("wall_1"))
+            {
+                car_1Move = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (car_1Move)
+        {
+            Car1Move();
         }
     }
 }
