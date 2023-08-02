f5a39e2fea96b72f85796f6bc8aea1d5665a6f5e
quangchinh186 <quangchinh1122@gmail.com>
2023-08-02 15:31:51 +0700

    adjust speed monitor, prepare to add traffic light

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class Control : MonoBehaviour
 {
@@ -11,32 +11,27 @@ public class Control : MonoBehaviour
     public Vector2 startPos;
     public float desiredDuration = 5.0f;
     public float eslapsedTime = 0.0f;
-
     public static float topBonusSpeed = 70.0f;
-
     private bool speedOn;
     private bool brakeOn;
     //public Time.deltaTime;
     [SerializeField]
     private AnimationCurve curve;
-    
     public float bonusSpeed = 0.0f;
-
     public float topSpeed = 0.0f;
     public float lowestSpeed = 0.0f;
+    public Text speedCounter;
 
     void Start()
     {
         speedOn = false;
         brakeOn = false;
-    
-
+        speedCounter.text = speed.ToString() + "km/h";
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if(Input.touchCount > 0)
         {
             //for(int i = 0; i < Input.touchCount; i++)
@@ -55,22 +50,18 @@ public class Control : MonoBehaviour
                         {
                             brakeOn = true;
                         }
-                        break;
-                    
+                        break;      
                     case TouchPhase.Ended:
                         brakeOn = speedOn = false;
                         topSpeed = bonusSpeed;
                         break;
                 }
-
-
             if(speedOn)
             {
                 eslapsedTime = eslapsedTime + Time.deltaTime > desiredDuration ? desiredDuration : eslapsedTime + Time.deltaTime;
                 float percentage = eslapsedTime / desiredDuration;
                 bonusSpeed = Mathf.Lerp(lowestSpeed, 70, curve.Evaluate(percentage));
             }
-
             if(brakeOn)
             {
                 bonusSpeed = bonusSpeed - 0.16f < -20 ? -20 :bonusSpeed - 0.16f;
@@ -81,31 +72,19 @@ public class Control : MonoBehaviour
                     eslapsedTime = 0;
                 }
             }
-
             speed = 20.0f + bonusSpeed;
         }
         else
         {
-            
             if(!speedOn &  bonusSpeed > 0) 
             {
                 eslapsedTime -= Time.deltaTime;
                 float percentage = eslapsedTime / desiredDuration;
                 bonusSpeed = Mathf.Lerp(topSpeed, 0, Mathf.SmoothStep(1,0,percentage));                   
-            }
-
-            
+            }      
             speed = 20.0f + bonusSpeed;
         }
-
-
-      
+        speedCounter.text = ((int)speed).ToString() + "km/h";
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
-        
     }
-
-
-
-
 }
