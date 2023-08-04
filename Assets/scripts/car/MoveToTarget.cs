6433310375bfc8849517e36f472eae3048ff662c
Tien Dung <dung14b@gmail.com>
2023-08-04 17:09:14 +0700

    add map with bridge

---
+++ Assets/scripts/car/MoveToTarget.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour {
+  [SerializeField]
+  GameObject target;
+  private isMove = false; 
+  float speed = 20.0f;
+
+  private void PlayerOnCheck() {
+    isMove = true;
+  }
+
+  private void Update() {
+    if (!target)
+    {
+      return;
+    }  
+    transform.LookAt(target.transform.position);
+    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+  }
+}
